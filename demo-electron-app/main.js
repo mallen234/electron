1d50c560343da49e613a005b0c604816b3eb3026
Marcus Allen <marcus.allen84@thehutgroup.com>
2024-02-09 21:07:31 +0000

    part 2 demo complete

--- demo-electron-app/main.js
+++ demo-electron-app/main.js
@@ -1,8 +1,18 @@
-const { app, BrowserWindow } = require("electron/main");
+const { app, BrowserWindow } = require("electron");
+const path = require("path"); // Add this line
+
+// app.on("ready", () => {
+//   app.applicationSupportsSecureRestorableState = function () {
+//     return true;
+//   };
+// });
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
   win.loadFile("index.html");
@@ -10,8 +20,4 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow();
-
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
 });
